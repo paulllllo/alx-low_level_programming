ce93efe7693b930743e87a45808e889929be098a
paulllllo <paulkelechi11@gmail.com>
2022-09-12 18:26:37 -0700

    A program that prints a string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/**
+ * main - This program prints the alphabets
+ *
+ * Return: Returns null
+ */
+
+int main(void)
+
+{
+
+	puts("Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
