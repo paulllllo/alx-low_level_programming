8292fdfe620b8c0b804cb0c81ebf208159c42b1f
paulllllo <paulkelechi11@gmail.com>
2022-09-12 19:30:49 -0700

    Aprogram that  prints the various sixes of different data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,195 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/**
+ * main - This program prints the alphabets
+ *
+ * Return: Returns null
+ */
+
+int main(void)
+
+{
+	char charType;
+	int intType;
+	long int longIntType;
+	long long int LLIntType;
+	float floatType;
+
+	printf("Size of a char: %zu byte(s),\n", sizeof(charType));
+	printf("Size of a int: %zu byte(s),\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s),\n", sizeof(longIntType));
+	printf("Size of a long long int: %zu byte(s),\n", sizeof(LLIntType));
+	printf("Size of a float: %zu byte(s),\n", sizeof(floatType));
+
+return (0);
+
+
+#include <stdlib.h>
+
+
+
+#include <time.h>
+
+
+
+#include <stdio.h>
+
+
+
+
+
+
+
+/**                                                                                                                                                                       
+
+ * main - This program prints the alphabets                                                                                                                               
+
+ *                                                                                                                                                                        
+
+ * Return: Returns null                                                                                                                                                   
+
+ */
+
+
+
+int main(void)
+	
+
+	
+{
+	
+        char charType;
+	
+        int intType;
+	
+        long int longIntType;
+	
+        long long int longLongIntType;
+	
+        float floatType
+		
+
+		
+                printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(intType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(longIntType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(longLongIntType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(floatType));#include <stdlib.h>
+									   
+
+									   
+#include <time.h>
+									   
+
+									   
+#include <stdio.h>
+									   
+
+									   
+
+									   
+
+									   
+/**                                                                                                                                                                       
+
+ * main - This program prints the alphabets                                                                                                                               
+
+ *                                                                                                                                                                        
+
+ * Return: Returns null                                                                                                                                                   
+
+ */
+									   
+
+									   
+									   int main(void)
+									   
+
+									   
+								   {
+									   
+									   char charType;
+									   
+									   int intType;
+									   
+									   long int longIntType;
+									   
+									   long long int longLongIntType;
+									   
+        float floatType
+		
+
+		
+                printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(intType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(longIntType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(longLongIntType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(floatType));#include <stdlib.h>
+									   
+
+									   
+#include <time.h>
+									   
+
+									   
+#include <stdio.h>
+									   
+
+									   
+
+									   
+
+									   
+/**                                                                                                                                                                       
+
+ * main - This program prints the alphabets                                                                                                                               
+
+ *                                                                                                                                                                        
+
+ * Return: Returns null                                                                                                                                                   
+
+ */
+									   
+
+									   
+									   int main(void)
+									   
+
+									   
+								   {
+									   
+									   char charType;
+									   
+									   int intType;
+									   
+									   long int longIntType;
+									   
+									   long long int longLongIntType;
+									   
+        float floatType
+		
+
+		
+                printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(intType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(longIntType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(longLongIntType));
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(floatType));
