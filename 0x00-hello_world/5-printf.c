76c586e2f5cfa65edc79db20e54b64030be16cb0
paulllllo <paulkelechi11@gmail.com>
2022-09-12 18:38:42 -0700

    A script that prints a string with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/**
+ * main - This program prints the alphabets
+ *
+ * Return: Returns null
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+
+}
