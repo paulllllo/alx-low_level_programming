cf754d29b32e0d8a8d52650888ac2c2ebdcc8d80
paulllllo <paulkelechi11@gmail.com>
2022-09-12 19:39:17 -0700

    Aprogram that  prints the various sixes of different data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,11 +21,11 @@ int main(void)
 	long long int LLIntType;
 	float floatType;
 
-	printf("Size of a char: %zu byte(s),\n", sizeof(charType));
-	printf("Size of a int: %zu byte(s),\n", sizeof(intType));
-	printf("Size of a long int: %zu byte(s),\n", sizeof(longIntType));
-	printf("Size of a long long int: %zu byte(s),\n", sizeof(LLIntType));
-	printf("Size of a float: %zu byte(s),\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(LLIntType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 return (0);
 
