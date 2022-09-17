43d2dd090d8451636619bd28067032ad872574f9
paulllllo <paulkelechi11@gmail.com>
2022-09-16 18:48:22 -0700

    Function that checks if a character is uppercase

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A project for the more functions, more loops ALX project
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int _isupper(int c);
+
+void _putchar (int arg);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+#endif /* _MAIN_H_ */
