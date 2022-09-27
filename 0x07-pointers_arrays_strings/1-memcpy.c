5b2c8dff939278e7a5bd112d17e298c0b774ffc3
paulllllo <paulkelechi11@gmail.com>
2022-09-26 21:02:13 -0700

    A function that copies bytes from one array to another array

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies the bytes stored in one array to another array
+ *
+ *@dest: A pointer to the address of the destination array
+ *@src: A pointer to the source array to be copied from
+ *@n: Number of bytes to be replaced
+ *
+ * Return: Returns the pointer 's'.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (s);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,8 @@
 
 
 
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 char *_memset(char *s, char b, unsigned int n);
 
 void _puts(char *a);
