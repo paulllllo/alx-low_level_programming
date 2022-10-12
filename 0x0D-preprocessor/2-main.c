d05e19348bfc0821397dfba2afd9f5a3d14d502e
paulllllo <paulkelechi11@gmail.com>
2022-10-11 21:30:23 -0700

    A program that prints its name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it was created from
+ *
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
