f01fdf368e74ea065f83654d0e0f29dd3433ec26
paulllllo <paulkelechi11@gmail.com>
2022-10-16 16:18:15 -0700

    A function that printsa name with a function passed to it

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name passed in as arg through an arg function
+ *
+ *@name: A string to be printed
+ *@f: A function pointe.
+ *
+ * Return: Returns void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
