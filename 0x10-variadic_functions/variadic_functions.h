f13a2d227dcf067016d1e0116646a018c4aace73
paulllllo <paulkelechi11@gmail.com>
2022-10-19 11:45:23 -0700

    A function that sums its args

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,42 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdarg.h>
+
+
+
+int sum_them_all(const unsigned int n, ...);
+
+void print_diagsums(int *a, int size);
+
+void print_chessboard(char (*a)[8]);
+
+char *_strstr(char *haystack, char *needle);
+
+char *_strpbrk(char *s, char *accept);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strchr(char *s, char c);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_memset(char *s, char b, unsigned int n);
+
+void _puts(char *a);
+
+int _strlen(char *s);
+
+int _isupper(int c);
+
+void _putchar (int arg);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+#endif /* _MAIN_H_ */
