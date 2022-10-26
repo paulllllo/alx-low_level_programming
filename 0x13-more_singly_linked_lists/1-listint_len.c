324150927c601ae3ba2e776d3ce031e700a65918
paulllllo <paulkelechi11@gmail.com>
2022-10-26 09:55:03 -0700

    A function that returns the number of elements in alist

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,27 @@
-#include <stddef.h>
+#include <stdlib.h>
 #include "lists.h"
 
-
 /**
- * listint_len - compute length of a linked list
- * @h: head of a linked list
- * Return: number of nodes of a linked list
+ * listint_ent - A function that counts all the elements of a list
+ * @h: the head of the linked list
+ *
+ * Return: Returns the number of nodes in the list.
  */
 
 size_t listint_len(const listint_t *h)
 {
-	size_t len;
+	size_t count = 0;
+	const listint_t *current = NULL;
 
-	for (len = 0; h; h = h->next, len++)
-		;
-	return (len);
+	current = h;
+
+	if (!h)
+		return (0);
+
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
 }
