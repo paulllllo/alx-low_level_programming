5badca55fe0d0ec4f6669a05683a0dd0df689cba
paulllllo <paulkelechi11@gmail.com>
2022-11-04 20:06:25 -0700

    [Corrected]

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,9 +31,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		wr = write(fd, "", strlen(""));
 	}
-	if (wr != strlen(text_content))
-		return (-1);
 	close(fd);
-
-	return (1);
+	if (wr > 0)
+		return (1);
+	else
+		return (-1);
 }
