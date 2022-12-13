7368a96fe5807a1b3989fc3736bdf12f98af188e
paul <paulkelechi11@gmail.com>
2022-12-12 16:21:17 -0800

    Created a shared library that performs basic functions in a python script

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,83 @@
+/**
+ * add - adds two integers
+ *
+ * @a - first int
+ * @b - second int
+ *
+ * Return: sum of ints.
+ */
+
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+
+/**
+ * sub - subtracts two integers
+ *
+ * @a - first int
+ * @b - second int
+ *
+ * Return: a - b
+ */
+
+
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a - first int
+ * @b - second int
+ *
+ * Return: a * b
+ */
+
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+
+/**
+ * div - div two integers
+ *
+ * @a - first int
+ * @b - second int
+ *
+ * Return: a / b
+ */
+
+
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+
+/**
+ * mod - returns remainder after dividing two integers
+ *
+ * @a - first int
+ * @b - second int
+ *
+ * Return: a % b
+ */
+
+
+int mod(int a, int b)
+{
+        return (a % b);
+}
